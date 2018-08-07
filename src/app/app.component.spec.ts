7558ed317dc83d8624a06a00e72d0269603979aa
louisdante9 <louisdante9@gmail.com>
2018-08-08 00:29:05 +0100

    (feat):create todo app -added todo class -add todo services -add unit testing for todo class -update app component to use todo services -add css styles to improve UI

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,34 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Todo } from './todo';
+
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule
+      ],
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
-  }));
+    });
+  });
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have a newTodo todo`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.newTodo instanceof Todo).toBeTruthy();
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('should display "Todos" in h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to todo-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Todos');
   }));
 });
